17c0768ba65865f8fa1c5948134a5adeb009e6ca
Kotaro Suzuki <kotarosz727@gmail.com>
2019-08-31 20:00:32 +0900

    create qiita api

--- js/template.js
+++ js/template.js
@@ -1,25 +1,13 @@
 var app = new Vue({
   el: "#app",
   data: {
-    km: 0,
-    m: 0,
-    mm: 0
+    firstName: "",
+    lastName: ""
   },
-  watch: {
-    km: function (value) {
-      this.km = value
-      this.m = value * 1000
-      this.mm = value * 1000000
-    },
-    m: function (value) {
-      this.km = value / 1000
-      this.m = value
-      this.mm = value * 1000
-    },
-    mm: function (value) {
-      this.km = value / 1000000
-      this.m = value / 1000
-      this.mm = value
+  computed: {
+    fullname: function () {
+      return this.firstName + '' +this.lastName
     }
   }
+
 })
