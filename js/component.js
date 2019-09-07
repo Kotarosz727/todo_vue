64ac69773c061c0a8ec5f5532fcc492f68f2b64d
Kotaro Suzuki <kotarosz727@gmail.com>
2019-09-07 15:51:24 +0900

    learn component and transition

---
+++ js/component.js
@@ -0,0 +1,16 @@
+// グローバルコンポーネント
+// Vue.component('hello-component', {
+//   template: '<p>Hello</p>'
+// })
+
+// // ローカルコンポーネント
+// var helloComponent = {
+//   template: '<p>hello</p>'
+// }
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    show: false
+  }
+})
